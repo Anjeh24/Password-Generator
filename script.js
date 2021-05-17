ee07c1c94c412665a19dacc7fdeab3efd007288d
Anjeh Mbah <junez24@yahoo.com>
2021-05-16 21:33:24 -0400

    Modified html and script.js to get functions to work properly

--- script.js
+++ script.js
@@ -1,84 +1,89 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
-var empty = "";
 var uCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lCase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "0123456789";
-var specialCharacters = "!'()*+,-./:;<=>?@[\]^_`{|}~#";
-
-var password = "";
+var specialCharacters = "!'()*+,-./:;?@[\]^_`{|}~#";
 var passwordcharacters = "";
-var passwordLength
+var passwordLength;
 
 
-function generatePassword() {
-    
 
-//Creating prompt to select length of password
+    function displayPassword(){
 
-passwordLength = prompt("How many charaters would you like your password to be? It has to be between 8 and 128 characters.");
- passwordLength = parseInt(passwordLength);
+    //Creating prompt to select length of password
 
-if (passwordLength < 8){
-    alert("Password must be atleast 8 characters");
-    return "";
-    
-}
- 
+     passwordLength = prompt("How many characters would you like your password to be? It has to be between 8 and 128 characters.");
 
-if (passwordLength > 128){
-    alert("Password cannot be more than 128 characters");
-    return "";
+     passwordLength = parseInt(passwordLength);
 
-}}
+     if (passwordLength >= 8 && passwordLength <= 128){
 
+    //Choosing/confirming options with yes or no boolean for lowercase
 
-//Choosing/confirming options with yes or no boolean for lowercase
+        var lowercaseOption = confirm("Add lowercase characters?");
 
-var lowercaseOption = confirm("Add lowercase characters?");
+       if (lowercaseOption) {
 
-if (lowercaseOption) {
-    passwordcharacters += lCase;
-}
+       passwordcharacters += lCase;
 
-//Choosing/confirming options with yes or no boolean for uppercase
+      }
 
-var uppercaseOption = confirm("Add uppercase characters?");
+      //Choosing/confirming options with yes or no boolean for uppercase
 
-if (uppercaseOption) {
-    passwordcharacters += uCase;
-}
+     var uppercaseOption = confirm("Add uppercase characters?");
+
+    if (uppercaseOption) {
+     passwordcharacters += uCase;
+    }
 
 //Choosing options (boolean) for numbers
 
-var numericalCharacters = confirm("Add numbers?");
+      var numericalCharacters = confirm("Add numbers?");
 
-if (numericalCharacters) {
-    passwordcharacters += numbers;
-}
+      if (numericalCharacters) {
+
+      passwordcharacters += numbers;
+
+      }
 
 //Boolean for adding special characters
-var specialSymbols = confirm("Add special characters?")
+         var specialSymbols = confirm("Add special characters?")
 
-if (specialSymbols) {
-    passwordcharacters += specialCharacters;
+        if (specialSymbols) {
+
+         passwordcharacters += specialCharacters;
+
+        }
+        }
+
+        else {
+
+        alert("Password cannot be less than 8 char or more than 128 characters");
+
+        passwordLength = prompt("How many charaters would you like your password to be? It has to be between 8 and 128 characters.");
+
+        passwordLength = parseInt(passwordLength);
 }
 
-for (var i = 0; i < passwordLength; i++) {
-    password = passwordcharacters[Math.floor(Math.random) * passwordcharacters.length]
-    console.log(password)
 }
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+         function generatePassword() {
 
-  passwordText.value = password;
+         let password=""
+
+         document.getElementById("password").innerHTML = "";
+
+         displayPassword();
+
+         for (var i = 0; i < passwordLength; i++) {
+
+         password += passwordcharacters.charAt(
+
+         Math.floor(Math.random() * Math.floor(passwordcharacters.length - 1)));
+}
+         var passwordText = document.getElementById("password");
+
+          passwordText.innerText= password;
 
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
